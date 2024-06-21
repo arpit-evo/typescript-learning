8890b9f2d0788b680584ebb31e7b1c6f85752f55
arpit-evo <arpit.m@evolutioncloud.in>
2024-06-21 16:49:46 +0530

    feat: learn interface

---
+++ oop-concepts/interfaces.js
@@ -0,0 +1,12 @@
+"use strict";
+class GoogleCalender {
+    constructor(name) {
+        this.name = name;
+    }
+    addEvent() {
+        throw new Error("Method not implemented.");
+    }
+    removeEvent() {
+        throw new Error("Method not implemented.");
+    }
+}
